730463b4da7b3d93aa13e1206204f4b6f5b3eb8b
framgia <framgia@framgia-Inspiron-3459>
2016-11-29 09:45:47 +0700

    init project

---
+++ src/main/java/hocviencntt/javaautomationcidemo/App.java
@@ -0,0 +1,13 @@
+package hocviencntt.javaautomationcidemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
